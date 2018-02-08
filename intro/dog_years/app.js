f1b1162236186c5cda44bcc71850f957b1f85faf
Maria Lobillo <maria.lobillo.santos@gmail.com>
2018-02-08 11:02:30 +0100

    dog years

---
+++ intro/dog_years/app.js
@@ -0,0 +1,18 @@
+// my Age
+let myAge = 36;
+// the earlyYears
+let earlyYears = 2;
+earlyYears *= 10.5;
+
+let laterYears = myAge - 2;
+
+laterYears *= 4;
+
+let myAgeInDogYears = earlyYears + laterYears;
+
+let myName = 'Maria';
+
+myName = myName.toLowerCase();
+
+
+console.log(`My name is ${myName}. I am ${myAgeInDogYears} years old in dog years.`);
