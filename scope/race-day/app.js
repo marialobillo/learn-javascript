028ac4ae29c45c665fbf7c62bfb9d75c6682225a
Maria Lobillo <maria.lobillo.santos@gmail.com>
2018-02-09 13:32:29 +0100

    scope

--- scope/race-day/app.js
+++ scope/race-day/app.js
@@ -1,50 +1,45 @@
-let event;
-const eventTrainingTimes = {'Marathon': 50, 'Triathlon': 100, 'Decathlon': 200 };
-
-/* this is used a few places and it is vulnerable */
 const getAllEvents = () => {
   return ['Marathon', 'Triathlon', 'Decathlon'];
 };
 
 const getRandomEvent = () => {
   const allEvents = getAllEvents();
-  event = allEvents[Math.floor(Math.random() * allEvents.length)];
+  const event = allEvents[Math.floor(Math.random() * allEvents.length)];
+  return event;
 };
 
 
-const getEventActivities = () => {
+const getEventActivities = (event) => {
   const allEvents = getAllEvents();
-
-
   if (!allEvents.includes(event)) {
     return null;
   }
 
+  let activities;
   if (event === 'Marathon') {
-    const activities = ['Running'];
-    return activities.join(', ');
+    activities = ['Running'];
   }
   if (event === 'Triathlon') {
-    const activities = ['Running', 'Cycling', 'Swimming'];
-    return activities.join(', ');
+    activities = ['Running', 'Cycling', 'Swimming'];
   }
   if (event === 'Decathlon') {
-    const activities = ['Running', 'Hurdles', 'Javelin throw', 'Discus Throw', 'Shot put', 'High Jump'];
-    return activities.join(', ');
+    activities = ['Running', 'Hurdles', 'Javelin throw', 'Discus Throw', 'Shot put', 'High Jump'];
   }
-
+  return activities.join(', ');
 };
 
-const getDaysToTrain = () => {
+const getDaysToTrain = event => {
   const allEvents = getAllEvents();
-
   if (!allEvents.includes(event)) {
     return null;
   }
-
+  const eventTrainingTimes = {'Marathon': 50, 'Triathlon': 100, 'Decathlon': 200 };
   return eventTrainingTimes[event];
 };
 
+const getEventMessage = () => {
+  const myEvent = getRandomEvent();
+  console.log('Your event is a ' + myEvent + '. Your event activities consist of ' + getEventActivities(myEvent) + '. You have ' + getDaysToTrain(myEvent) +  ' days to train.');
+}
 
-getRandomEvent();
-console.log('Your event is a ' + event + '. Your event activities consist of ' + getEventActivities() + '. You have ' + getDaysToTrain() +  ' days to train.');
+getEventMessage();
