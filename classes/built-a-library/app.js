3e838c5a1cddd08f37b267751c66b4cd0425f032
Maria Lobillo <maria.lobillo.santos@gmail.com>
2018-02-14 13:01:57 +0100

    book class

--- classes/built-a-library/app.js
+++ classes/built-a-library/app.js
@@ -30,3 +30,34 @@ class Media{
   }
 
 }
+
+class Book extends Media{
+  constructor(author, title, pages){
+    this.author = author;
+    this.title = title;
+    this.pages = pages;
+    super(isCheckedOut);
+    super(ratings);
+  }
+  get author(){
+    return this.author;
+  }
+  get title(){
+    return this.title;
+  }
+  get pages(){
+    this.pages;
+  }
+  getAverageRating(){
+  	let averageSum = this.ratings.reduce((currentSum, rating) => currentSum + rating, 0) / rating.length;
+  }
+  toggleCheckOutStatus(){
+    this._isCheckedOut ? this._isCheckedOut=false : this._isCheckedOut=true;
+  }
+  addRating(inputValue){
+    this.ratings.push(inputValue);
+  }
+
+}
+
+let myBook = new Book('Bill Bryson', 'A Short History of Nearly Everything', 544);
